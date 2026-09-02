2432d1872480dd3893ba1e6f271f367fffbd1003
Gabriel-B1306 <bunalesjohngabriel@gmail.com>
2026-09-02 18:40:53 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,5 @@
+phrase1 = "\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t      *   *  Hi, My Name is John Gabriel A. Buñales   *   *\n\t\t\t\t\t\t      *   *  \t\t   From BSIT-2C\t              *   *\n\t\t\t\t\t\t      *   *  \t\t\t\t\t      *   *\n\t\t\t\t\t\t      *   *    *    *     *    *     *     *    *     *   * \n\t\t\t\t\t\t      *   *    *    *     *    *     *     *    *     *   * "
+
+
+
+print(phrase1)
