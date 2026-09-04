bf8bdcce1ba43d45b39442070bfa1c0d0fe20d17
Gabriel-B1306 <bunalesjohngabriel@gmail.com>
2026-09-04 19:34:38 +0800

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,19 @@
+# Operator Activity 5
+a = 7
+b = 5
+
+sum = a + b
+difference = a - b
+multiplication = a * b
+division = a / b
+floor_division = a // b
+modulus = a % b
+exponent = a ** b
+
+print("The sum of ", a, " and", b, "is", sum)
+print("The difference of ", a, " and", b, "is", difference)
+print("The multiplication of ", a, " and", b, "is", multiplication)
+print("The division of ", a, " and", b, "is", division)
+print("The floor division of ", a, " and", b, "is", floor_division)
+print("The modulus of ", a, " and", b, "is", modulus)
+print("The exponent of ", a, " and", b, "is", exponent)
